221960a99801e93310c5bea262ae8988802e99a6
keluryinfutao <yinfutao@kelur.com>
2016-01-14 18:15:26 +0800

    test change

--- mypackage/sk.py
+++ mypackage/sk.py
@@ -10,3 +10,15 @@ def test2(name,state=None):
 
     """
     return 0
+def test3(name,state=None):
+    """This function is a test.
+
+    :param name: The name to use.
+    :type name: str.
+    :param state: Current state to be in .
+    :type state: bool.
+    :returns: int --the return code.
+    :raises: AttributeError , KeyError
+
+    """
+    return 0
